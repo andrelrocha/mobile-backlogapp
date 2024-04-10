1e4eb9a36196b0923332b3ededbf9f4f26e7bbd1
andrelrocha <andrerocha0911@gmail.com>
2024-04-10 12:35:08 -0300

    useReducer para controlar seleção

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import { SafeAreaView, View } from 'react-native';
 import Create from './src/screens/User/Create';
 import { styles } from './styles';	
 import Header from './src/components/header';
+import ListUser from './src/screens/User/ListUser';
 
 function App() {
   return (
     <View style={styles.body}>
       <SafeAreaView>
         <Header />
+        <ListUser />
         {/* <Login /> */}
         <Create />
       </SafeAreaView>
--- src/screens/User/ListUser/index.tsx
+++ src/screens/User/ListUser/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import { ApiManager } from "../../../utils/API-axios/ApiManager"
 import { FlatList, Text, View } from "react-native"
+import { TouchableOpacity } from "react-native";
 import Header from "../../../components/header"
 
 export default function Users() {
@@ -8,17 +9,35 @@ export default function Users() {
     const [login, setLogin] = useState('')
     const [name, setName] = useState('')
 
+    //const [selected, setSelected] = useState(false)
+    const [selected, invertSelected] = useReducer(
+        (selected) => {
+            console.log('useReducer')
+            console.log(selected)
+            return !selected;
+        }, false
+    );
+
     useEffect(() => {
-        ApiManager.get('users').then((response) => {
+        console.log('useEffect')
+        /*
+        ApiManager.get('/users').then((response) => {
             console.log(response.data)
             setId(response.data.id)
             setLogin(response.data.login)
             setName(response.data.name)
         })
+        */
     }, [])
+
+ 
 
     return (
         <View>
+            <TouchableOpacity onPress={invertSelected}>
+                <Text>Selecionar</Text>
+            </TouchableOpacity>
+
             <FlatList
                 data={[{ key: id, login, name }]}
                 renderItem={({ item }) => <Text>{item.key} - {item.login} / {item.name}</Text>}
